4a47429eca611c6da701e976b3f2b04baf0aaa03
Naginagi-chan <company0930@gmail.com>
2026-09-26 21:18:15 +0900

    feat: 인스턴스 값의 변환

--- OOP1/src/Tv.java
+++ OOP1/src/Tv.java
@@ -1,9 +1,25 @@
-public class Tvs
+class Tvs
 {
-    int tvChannel;
+    String color;
+    boolean power;
+    int channel;
 
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
 }
 
-void main() {
-    
+class TvTest2 {
+    void main() {
+        Tvs t1 = new Tvs();
+        Tvs t2 = new Tvs();
+        System.out.println("t1 channel " + t1.channel + ".");
+        System.out.println("t2 channel " + t2.channel + ".");
+
+        t1.channel = 7;
+        System.out.println("change t1 channel to 7");
+
+        System.out.println("t1 channel " + t1.channel + ".");
+        System.out.println("t2 channel " + t2.channel + ".");
+    }
 }
